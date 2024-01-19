d8e7a19bb5cab4c09bbba326ba3eb8e6301eabd5
Yusuf Mendeş <yusufmendes123@gmail.com>
2024-01-19 16:09:28 +0300

    Add alignment function

--- app/src/main/java/com/yusufmendes/jetpackcomposedesignwork/MainActivity.kt
+++ app/src/main/java/com/yusufmendes/jetpackcomposedesignwork/MainActivity.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -34,8 +35,9 @@ class MainActivity : ComponentActivity() {
                     /*UseOfRow()
                     UseOfColumn()
                     UseOfBox()
-                    UseMixed()*/
-                    UseOfArrangement()
+                    UseMixed()
+                    UseOfArrangement()*/
+                    UseOfAlignment()
                 }
             }
         }
@@ -191,6 +193,49 @@ fun UseOfArrangement() {
     }
 }
 
+@Composable
+fun UseOfAlignment() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Box(
+            modifier = Modifier
+                .size(100.dp)
+                .background(Color.Red)
+        )
+        Box(
+            modifier = Modifier
+                .size(80.dp)
+                .background(Color.Green)
+        )
+        Box(
+            modifier = Modifier
+                .size(60.dp)
+                .background(Color.Blue)
+        )
+    }
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Box(
+            modifier = Modifier
+                .size(100.dp)
+                .background(Color.Red)
+        )
+        Box(
+            modifier = Modifier
+                .size(80.dp)
+                .background(Color.Green)
+        )
+        Box(
+            modifier = Modifier
+                .size(60.dp)
+                .background(Color.Blue)
+        )
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
@@ -198,7 +243,8 @@ fun GreetingPreview() {
         /*UseOfRow()
         UseOfColumn()
         UseOfBox()
-        UseMixed()*/
-        UseOfArrangement()
+        UseMixed()
+        UseOfArrangement()*/
+        UseOfAlignment()
     }
 }
